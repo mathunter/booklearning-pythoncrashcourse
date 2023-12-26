02f2dc3d081481e90ca5b036df59adabfa876271
Mat Hunter <mathew.hunter@gmail.com>
2023-12-26 17:33:19 -0500

    Added settings, ship
    - Added a class to manage the game settings.
    - Added a class to manage the display of the ship.
    - Updated the game to render the ship at a starting location.

--- alien-invasion/alien_invasion.py
+++ alien-invasion/alien_invasion.py
@@ -1,6 +1,10 @@
 import sys
 import pygame
 
+from settings import Settings
+from ship import Ship
+
+
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
 
@@ -10,11 +14,21 @@ class AlienInvasion:
         # Initialize the game framework
         pygame.init()
 
+        # Create a game clock
+        self.clock = pygame.time.Clock()
+
+        # Create a new settings instance
+        self.settings = Settings()
+
         # Create the game window
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
 
         # Set the screen title
         pygame.display.set_caption("Alien Invasion")
+
+        # Create the ship
+        self.ship = Ship(self)
 
     def run_game(self):
         """Start the main loop for the game"""
@@ -27,8 +41,17 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            # Redraw the screen during each pass through the loop
+            self.screen.fill(self.settings.bg_color)
+
+            # Add the ship
+            self.ship.blitme()
+            
             # Make the most recently-drawn screen visible
             pygame.display.flip()
+
+            # Run the game clock
+            self.clock.tick(60)
 
 if __name__ == "__main__":
 
